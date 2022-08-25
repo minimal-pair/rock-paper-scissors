4971b40544b153d38eb51f5bb714b25e48f1cdda
Tim Austin <tja.xpprime@gmail.com>
2022-08-24 20:53:27 -0500

    add buttons css

--- code.js
+++ code.js
@@ -41,44 +41,4 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // play game 5 times
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Rock, paper, or scissors?");
-        playerChoice = playerChoice.toLowerCase();
-
-        let computerChoice = getComputerChoice();
-
-        result = playRound(playerChoice, computerChoice);
-        switch (result) {
-            case 0: // tie
-                console.log("Tie!");
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-                break;
-            case 1: // win
-                console.log(`You win! ${capitalize(playerChoice)} beats ${computerChoice}`);
-                playerScore += 1;
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-                break;
-            case 2: // lose
-                console.log(`You lose! ${capitalize(computerChoice)} beats ${playerChoice}`)
-                computerScore += 1;
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-                break;
-        }
-    }
-
-    // print final message and scores
-    if (playerScore === computerScore) {
-        console.log(`Tie :/ You both won ${playerScore}/5 games.`);
-    } else if (playerScore > computerScore) {
-        console.log(`You are the winner! You won ${playerScore}/5 games.\nThe computer won ${computerScore}/5.`);
-    } else {
-        console.log(`You are the loser! You won ${playerScore}/5 games.\nThe computer won ${computerScore}/5.`);
-    }
-}
-
-game();
+const buttons = document.querySelectorAll(".button");
