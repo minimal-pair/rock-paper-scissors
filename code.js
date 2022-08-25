6f50896b9e0fc6839610e4a193b392dcca6c665e
Tim Austin <tja.xpprime@gmail.com>
2022-08-25 11:22:27 -0500

    add event listeners and images of player/computer selections

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+// Colors
+const white = "aliceblue";
+const green = "lightgreen";
+const red = "lightcoral";
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -16,6 +21,27 @@ function getComputerChoice() {
     }
 }
 
+function modifyScore(textElement, modifier) {
+    let text = textElement.textContent;
+    let score = Number(text[text.length - 1]);
+    let startText = text.slice(0, -1);
+    console.log(startText);
+    score += modifier;
+    textElement.textContent = startText + score;
+}
+
+function displayPlayerImage(playerSelection, color) {
+    playerImage.style.background = color;
+    playerImage.firstElementChild.src = `images/160${playerSelection}.png`;
+    playerImage.firstElementChild.style.cssText = "height: 100%; width: 100%; object-fit: cover;";
+}
+
+function displayComputerImage(computerSelection, color) {
+    computerImage.style.background = color;
+    computerImage.firstElementChild.src = `images/160${computerSelection}.png`;
+    computerImage.firstElementChild.style.cssText = "height: 100%; width: 100%; object-fit: cover;";
+}
+
 function capitalize(str) {
     let firstLetter = str.slice(0, 1);
     let restOfWord = str.slice(1);
@@ -25,20 +51,45 @@ function capitalize(str) {
     return firstLetter + restOfWord;
 }
 
-function playRound(playerSelection, computerSelection) {    
+function playRound(e) {
+    const playerSelection = e.target.parentElement.id;
+    const computerSelection = getComputerChoice();
+
+
+    
     // tie
     if (playerSelection === computerSelection) {
-        return 0
+        displayPlayerImage(playerSelection, white);
+        displayComputerImage(computerSelection, white);
     // player wins
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return 1;
+        displayPlayerImage(playerSelection, green);
+        displayComputerImage(computerSelection, red);
+        modifyScore(playerText, 1);
     // player loses
     } else {
-        return 2;
+        displayPlayerImage(playerSelection, red);
+        displayComputerImage(computerSelection, green);
+        modifyScore(computerText, 1);
     }
 }
 
 const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    button.addEventListener("click", playRound);
+});
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const playerImage = document.querySelector("#you-img");
+const computerImage = document.querySelector("#comp-img");
+
+const playerText = document.querySelector("#you-score");
+const computerText = document.querySelector("#comp-score");
+
+
